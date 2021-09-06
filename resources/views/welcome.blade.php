4ac2ddbabdcd9933e387962385c36a0079c69263
RadenDimasErlangga21 <zirkified123@gmail.com>
2021-09-06 13:59:24 +0700

    menampilkan nama mahasiswa pada welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>RADEN DIMAS ERLANGGA</title>
+        <title> RADEN DIMAS ERLANGGA </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
